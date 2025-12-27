5339437facc36ac418a11d32613c36d9fc3a3d1e
LIza <lizasusoreva292@gmail.com>
2025-12-28 01:25:40 +0200

    task25

---
+++ tests/TASK25/checkEmailField/borderColorRed.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+import { RegistrationPage } from "../../auth/registration.page.js";
+
+test("Email border is red for invalid values", async ({ page }) => {
+  const registration = new RegistrationPage(page);
+
+  await registration.open();
+
+  await registration.fillEmail("k");
+  await expect(page.locator("text=Email is incorrect")).toBeVisible();
+
+  await registration.fillEmail("ThisNameIsWayTooLongToBeValid");
+  await registration.expectRedBorder(registration.email);
+});
