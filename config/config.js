719b89169069e054bf49106dc04bd1eb3ef1a847
LIza <lizasusoreva292@gmail.com>
2026-02-25 17:17:23 +0200

    changes

---
+++ config/config.js
@@ -0,0 +1,10 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+//
+// export default {
+//   baseUrl: process.env.BASE_URL,
+//   credentials: {
+//     userName: process.env.HTTP_CREDENTIALS_USERNAME,
+//     userPassword: process.env.HTTP_CREDENTIALS_PASSWORD,
+//   },
+// };
